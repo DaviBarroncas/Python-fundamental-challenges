3225fe4db28478605028f621ea3352fadc5a4c68
DaviBarroncas <davi12bdc@gmail.com>
2026-03-18 15:49:12 -0300

    exercício atualizado

---
+++ ex 88.py
@@ -0,0 +1,20 @@
+from random import randint
+print("MEGA SENA: SORTEIO")
+a=list()
+jogos=list()
+b=int(input("quantas vezes voce quer sortear? "))
+c=1
+while c <=b:
+    d=0
+    while True:
+        e=randint(1, 60)
+        if e not in a:
+            a.append(e)
+            d+=1
+        if d>=6:
+            break
+    a.sort()
+    jogos.append(a[:])
+    a.clear()
+for i, l in enumerate(jogos):
+    print(f"jogo {i+1}: {l}")
