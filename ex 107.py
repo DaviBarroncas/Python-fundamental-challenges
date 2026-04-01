3621a9aa2ba4ecd9d9ad2cbe3f92733b323d1308
DaviBarroncas <davi12bdc@gmail.com>
2026-04-01 17:42:20 -0300

    subindo o exercício 107

---
+++ ex 107.py
@@ -0,0 +1,6 @@
+import uteis
+p=float(input("digite o preço R$: "))
+print(f"a metade de {p} é {uteis.metade(p)}")
+print(f"o dobro de {p} é {uteis.dobre(p)}")
+print(f"Aumentando 10%, temos {uteis.aumentar(p)}")
+print(f"reduzindo 13%, temos {uteis.reduzir(p)}")
---
+++ uteis.py
@@ -0,0 +1,14 @@
+def metade(p):
+    return p/2
+
+
+def aumentar(p):
+    return p*(11/10)
+
+
+def dobre(p):
+    return p*2
+
+
+def  reduzir(p):
+    return p*(87/100)
